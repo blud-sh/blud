19a4d07f8aed7838a3b8d36906ea603b04740680
Aditya <aditya.chaudhary0523@gmail.com>
2024-11-30 19:54:40 +0530

    fix: fix scrollbar disappearing on theme toggler

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from 'lucide-react';
+import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -16,7 +16,7 @@ export function ModeToggle() {
     const { setTheme } = useTheme();
 
     return (
-        <DropdownMenu>
+        <DropdownMenu modal={false}>
             <DropdownMenuTrigger asChild>
                 <Button
                     variant="outline"
