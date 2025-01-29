501a359b1c530cd15a24442429faeaaa3d1da7cb
Rachit Srivastava <honey032004@gmail.com>
2025-01-30 03:01:05 +0530

    refactor: Added Link to SignIN

--- src/components/shared/header-auth.tsx
+++ src/components/shared/header-auth.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import { Button } from '@/components/ui/button';
-import '@/app/styles/some.css';
+"use client";
+
+import React from "react";
+import { Button } from "@/components/ui/button";
+import "@/app/styles/some.css";
+import Link from "next/link";
 
 export default function HeaderAuth() {
-    return (
-        <div>
-            <Button
-                variant="outline"
-                className="button--calypso w-[90px] h-[45px] bg-[#D9D9D9] text-[#000000] rounded-[39px] border-0 flex items-center justify-center gap-2 transition-colors duration-300 hover:bg-[#BDBDBD] whitespace-nowrap"
-            >
-                <span className="flex items-center">Sign In</span>
-            </Button>
-        </div>
-    );
+  return (
+    <div>
+      <Link href="/login">
+        <Button
+          variant="outline"
+          className="button--calypso w-[90px] h-[45px] bg-[#D9D9D9] text-[#000000] rounded-[39px] border-0 flex items-center justify-center gap-2 transition-colors duration-300 hover:bg-[#BDBDBD] whitespace-nowrap"
+        >
+          <span className="flex items-center">Sign In</span>
+        </Button>
+      </Link>
+    </div>
+  );
 }
